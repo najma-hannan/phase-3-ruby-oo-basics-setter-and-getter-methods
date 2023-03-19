37b0b7936a835309c4353ee467fcedf2ada3eb75
Hannan <najma.mahat@student.moringaschool.com>
2023-03-19 09:04:23 +0300

    completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 class Person
+
+    def name=(value)
+        @name = value
+    end
+
+    def name
+        @name
+    end
+
+    def job=(value)
+        @job = value
+    end
+
+    def job
+        @job
+    end
 
 end
